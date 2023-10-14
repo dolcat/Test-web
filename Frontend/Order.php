1863ff4bc9f395998008375f90c2050df6b32faf
ducloc1st <143262937+ducloc1st@users.noreply.github.com>
2023-10-14 16:11:59 +0700

    Update Order.php

--- Frontend/Order.php
+++ Frontend/Order.php
@@ -1,28 +1,76 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Đơn hàng</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- Main CSS-->
-<link rel="stylesheet" type="text/css" href="main.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-</head>
-
-<body>
+<?php include("header.php");
+?>
 	<!---------------------- Order --------------------->
-	<section class="order">
+	<?php
+	$count = 0;
+	$conn=mysqli_connect("localhost","root","") or die ("Không connect đc với máy chủ");
+	//Chọn CSDL để làm việc
+	mysqli_select_db($conn,"qlsach") or die ("Không tìm thấy CSDL");
+	//Tạo câu truy vấn
+	$sql_select_order="Select * from `order`, `book_list` where `order`.`ID_book` = `book_list`.`ID_book`";
+	$result_order=mysqli_query($conn,$sql_select_order);
+	$tong_bg=mysqli_num_rows($result_order);// đếm số bản ghi
+		//echo $tong_bg; die;
+	$stt_order=0;
+	while($row=mysqli_fetch_object($result_order))
+	{
+		$stt_order++;
+		$ID_order[$stt_order]=$row->ID_order;
+		$title[$stt_order]=$row->title;
+		$number_cart[$stt_order] = $row->number_cart;
+		$price[$stt_order] = $row->price;
+		$Total_price[$stt_order] = $row->Total_price;
+	}
+	?>
+	<section>
 		<div class="container">
-			<div class="order-menu">
-				<li><a href="">Tất cả đơn</a></li>
-				<li><a href="">Chờ thanh toán</li>
-				<li><a href="">Đang xử lí</li>
-				<li><a href="">Đang vận chuyển</li>
-				<li><a href="">Đã giao</li>
-				<li><a href="">Đã hủy</li>
+		<div class="order-cotent-row">
+			<div style="margin-left: 30px" class="cart-cotent-left ">
+				<table style="width:100%" border="1" style="border-collapse: collapse;">
+				  <tr>
+					<th>STT</th>
+					<th>Mã đơn hàng</th>
+					<th>Tên sản phẩm</th>
+					<th>Số lượng</th>
+					<th>Đơn giá</th>
+					<th>Thành tiền</th>
+				  </tr>
+					<?php
+	  for ($i=1;$i<=$tong_bg;$i++)
+	  {
+	  ?>
+    <tr>
+      	<td><?php echo $i;?></td>
+      	<td><?php echo $ID_order[$i] ?></td>
+      	<td><?php echo $title[$i]?></td>
+		<td><?php echo $number_cart[$i] ?></td>
+		<td><?php echo $price[$i] ?></td>
+		<td><?php echo $Total_price[$i]?></td>
+    </tr>
+	  <?php
+	  }
+	  ?>
+				</table>
 			</div>
 		</div>
+	</div>
 	</section>
+<!-- footer -->
+    <footer>
+        <div class="footer-item">
+            <div class="img-footer">
+                <img src="img/logo.png" alt="" style="width: 120px;height: 139px;" />
+            </div>
+            <div class="social-footer">
+                <li class="footer_icon"><a target="_blank" href="https://www.facebook.com/thanhlongdev">
+                        <i class="fa-brands fa-facebook"></i>
+                    </a></li>
+                <li><a target="_blank" href="https://github.com/long1211">
+                        <i class="fa-brands fa-github"></i>
+                    </a></li>
+
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
