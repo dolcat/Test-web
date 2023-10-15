479799fd4c0e904e27974517c62a4f841251f483
ducloc1st <143262937+ducloc1st@users.noreply.github.com>
2023-10-15 18:10:34 +0700

    Update footer.php

--- Frontend/footer.php
+++ Frontend/footer.php
@@ -1,82 +1,39 @@
+<!doctype html>
+<html>
 <head>
-    <link rel="stylesheet" href="style.css">
+<meta charset="utf-8">
+<link type="text/css" rel="stylesheet" href="footer.css">
 </head>
 
 <body>
-    <div id="footer">
-        <div class="lienhe">
-            <ul class="lh1">
-                <li><a href="">THÔNG TIN HỖ TRỢ</a>
-                    <ul class="lh2">
-                        <li><a href="">Tìm hiểu về mua trả góp</a></li>
-                        <li><a href="">Tìm trung tâm bảo hành hãng</a></li>
-                        <li><a href="">Hướng dẫn mua hàng online</a></li>
-                        <li><a href="">Giao hàng & thanh toán</a></li>
-                    </ul>
-
-                </li>
-                <li><a href="">DỊCH VỤ KHÁCH HÀNG</a>
-                    <ul class="lh2">
-                        <li><a href="">Giới thiệu về công ty</a></li>
-                        <li><a href="">Tuyển dụng</a></li>
-                        <li><a href="">Gửi góp ý & khiếu nại</a></li>
-                        <li><a href="">Tìm siêu thị</a></li>
-                        <li><a href="">Liên hệ</a></li>
-                    </ul>
-
-                </li>
-                <li><a href="">DỊCH VỤ & HỖ TRỢ</a>
-                    <ul class="lh2">
-                        <li><a href="">Chính sách giao hàng</a></li>
-                        <li><a href="">Chính sách đổi sản phẩm</a></li>
-                        <li><a href="">Chính sách bảo hành</a></li>
-                        <li><a href="">Chính sách trả góp</a></li>
-                        <li><a href="">Chính sách đổi trả</a></li>
-                    </ul>
-
-                </li>
-                <li><a href="">BẢN TIN</a>
-                    <p>Đăng kí thành viên để nhận bản tin mỗi ngày</p>
-                    <div class="mail">
-                        <input type="text" placeholder="Nhập email của bạn">
-                        <Button>Gửi</Button>
-                    </div>
-                </li>
-            </ul>
-
-        </div>
-        <div class="diachi">
-            <div class="diachi-left">
-                <div class="logo">
-                </div>
-            </div>
-            <div class="diachi-mid">
-                <div class="mid-comtent">
-                    <i class="ti-location-pin"></i>
-                    <p>Địa chỉ: Tân Triều, Thanh Trì, Hà Nội</p>
-                </div>
-                <div class="mid-comtent">
-                    <i class="ti-headphone-alt"></i>
-                    <p>Điện thoại : 0328443736</p>
-
-                </div>
-                <div class="mid-comtent">
-                    <i class="ti-email"></i>
-                    <p>Email: huatunglam1205@gmail.com</p>
-
-                </div>
-
-            </div>
-            <div class="diachi-right">
-                <div class="icon"><a href=""><i class="ti-facebook"></i></a></div>
-                <div class="icon"><a href=""><i class="ti-instagram"></i></a></div>
-                <div class="icon"><a href=""><i class="ti-youtube"></i></a></div>
-                <div class="icon"><a href=""><i class="ti-google"></i></a></div>
-                <div class="icon"><a href=""><i class="ti-twitter"></i></a></div>
-
-            </div>
-
-        </div>
-
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <h4>Giới thiệu</h4>
+        <p>
+          Website bán sách trực tuyến của chúng tôi chuyên cung cấp các đầu sách mới nhất, hot nhất trên thị trường.
+        </p>
+      </div>
+      <div class="col-md-4">
+        <h4>Liên hệ</h4>
+        <ul>
+          <li>
+            <p>Địa chỉ: Tân Triều, Thanh Trì, Hà Nội</p>
+          </li>
+          <li>
+            <p>Điện thoại : 0972594823</p>
+          </li>
+          <li>
+            <p>Email: kieuducloc2003@gmail.com</p>
+          </li>
+        </ul>
+      </div>
     </div>
+    <div class="copyright">
+      	<p>Copyright &copy; 2023 Website playbook</p>
+    </div>
+  </div>
+</footer>
 </body>
+</html>
