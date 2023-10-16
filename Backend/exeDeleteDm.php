a3172749cd48fffc2641120eb00f3724fcf8f619
ducloc1st <143262937+ducloc1st@users.noreply.github.com>
2023-10-16 15:02:06 +0700

    Add files via upload

---
+++ Backend/exeDeleteDm.php
@@ -0,0 +1,15 @@
+<?php 
+include("connect.php");
+if(isset($_GET['id_danhmuc'])){
+    $id_danhmuc = $_GET['id_danhmuc'];
+    $sqlDeleteDm = "DELETE FROM `danhmuc` WHERE id_danhmuc = '$id_danhmuc'";
+    $check = mysqli_query($conn, $sqlDeleteDm);
+    if($check == true){
+        mysqli_close($conn);
+        header("location: quanlydanhmuc.php");
+    }
+}
+else{
+    echo "Ko co";
+}
+?>
